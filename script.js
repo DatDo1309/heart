f822bdc29f6094e6ef5c246a3a83bd59a866637f
DatDo1309 <63733095+DatDo1309@users.noreply.github.com>
2022-11-08 21:59:09 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   
   var vid = $('video');
-  var vid_w_orig = 1280;
-	var vid_h_orig = 720;
+  var vid_w_orig = 3840;
+	var vid_h_orig = 2160;
 
   // re-scale image when viewport resizes
 	$(window).resize(function(){
